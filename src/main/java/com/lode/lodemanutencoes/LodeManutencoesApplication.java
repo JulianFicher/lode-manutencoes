4a18ec873b81e97b76d39235b52813f90e5540ca
JulianFicher <juficher@outlook.com>
2026-06-02 18:12:15 -0300

    Adiciona Estrutura inicial

---
+++ src/main/java/com/lode/lodemanutencoes/LodeManutencoesApplication.java
@@ -0,0 +1,13 @@
+package com.lode.lodemanutencoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodeManutencoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LodeManutencoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lode/lodemanutencoes/LodeManutencoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lode.lodemanutencoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodeManutencoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
